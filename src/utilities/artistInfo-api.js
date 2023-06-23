df8ad9d8ad968a3f2b5dc6f8f6af1d68910b534b
nandobrain <prf.arturo@gmail.com>
2023-06-23 12:53:45 -0700

    artist info api

--- src/utilities/artistInfo-api.js
+++ src/utilities/artistInfo-api.js
@@ -2,4 +2,8 @@ import sendRequest from "./send-request";
 const BASE_URL = '/api/artistInfo';
 
 export function editArtistInfo(userId)
-  return sendRequest(`${BASE_URL}/work${userId}`, 'POST');
+return sendRequest(`${BASE_URL}`, 'POST', {artistInfo});
+
+  export async function getArtistsInfo() {
+    return sendRequest(`${BASE_URL}`)
+  }
